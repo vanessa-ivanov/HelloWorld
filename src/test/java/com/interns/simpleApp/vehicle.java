e2ff3a8dfda99468f1086b72ef022e217c5bf504
Thomas Dallamassl <Thomas.Dallamassl@A1.at>
2024-07-10 10:25:44 +0200

    added vehicle class for vehicle data

---
+++ src/test/java/com/interns/simpleApp/vehicle.java
@@ -0,0 +1,35 @@
+package com.interns.simpleApp;
+
+public class vehicle {
+
+    private String storage;
+    private String type;
+    private String fuel;
+    private String size;
+
+    public void setStorage(String storage) {
+        this.storage = storage;
+    }
+    public String getStorage() {
+        return storage;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+    public String getFuel() {
+        return fuel;
+    }
+    public void setSize(String size) {
+        this.size = size;
+    }
+    public String getSize() {
+        return size;
+    }
+
+}
