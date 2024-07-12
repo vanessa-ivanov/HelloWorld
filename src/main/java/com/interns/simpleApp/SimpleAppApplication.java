bb769dfdd3ee0da7e6b2cbb860b3f8ec68924a44
Thomas Dallamassl <Thomas.Dallamassl@A1.at>
2024-07-12 15:14:35 +0200

    minor changes in SimpleAppApplication

--- src/main/java/com/interns/simpleApp/SimpleAppApplication.java
+++ src/main/java/com/interns/simpleApp/SimpleAppApplication.java
@@ -1,6 +1,5 @@
 package com.interns.simpleApp;
 
-import com.interns.simpleApp.controller.IndexController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
