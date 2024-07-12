7d8c547114f952d33b46f7b12a5ac3d908260bb9
Thomas Dallamassl <Thomas.Dallamassl@A1.at>
2024-07-12 16:13:17 +0200

    added user storage system

--- src/main/java/com/interns/simpleApp/controller/IndexController.java
+++ src/main/java/com/interns/simpleApp/controller/IndexController.java
@@ -1,11 +1,16 @@
 package com.interns.simpleApp.controller;
 
+import com.interns.simpleApp.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+    User[] users = new User[10];
+    int userNumber = 0;
 
     @GetMapping("/")
     public String index() {
@@ -15,6 +20,20 @@ public class IndexController {
     @RequestMapping("/register")
     public String userRegistration(){
         return "register";
+    }
+
+    @RequestMapping("/registerInput")
+    public String userRegistrationInput(@ModelAttribute User user, Model model) {
+        users[userNumber] = new User(user.getFname(),
+                user.getLname(),
+                user.getEmail(),
+                user.getPasswd(),
+                user.getDob(),
+                user.getGender()
+        );
+        userNumber += 1;
+        System.out.println(user.toString());
+        return "welcome";
     }
 
     @RequestMapping("/login")
