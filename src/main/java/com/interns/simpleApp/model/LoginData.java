9345b53836d39debed19f322d224968756331866
Thomas Dallamassl <Thomas.Dallamassl@A1.at>
2024-07-15 09:37:50 +0200

    added LoginData class and content

---
+++ src/main/java/com/interns/simpleApp/model/LoginData.java
@@ -0,0 +1,24 @@
+package com.interns.simpleApp.model;
+
+public class LoginData {
+    private String logEmail;
+    private String logPasswd;
+
+    public LoginData(String logEmail, String logPasswd) {
+        this.logEmail = logEmail;
+        this.logPasswd = logPasswd;
+    }
+
+    public String getLogEmail() {
+        return logEmail;
+    }
+    public void setLogEmail(String logEmail) {
+        this.logEmail = logEmail;
+    }
+    public String getLogPasswd() {
+        return logPasswd;
+    }
+    public void setLogPasswd(String logPasswd) {
+        this.logPasswd = logPasswd;
+    }
+}
