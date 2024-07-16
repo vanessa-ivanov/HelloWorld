ba35a0e89149fe055b359a6dd15a14c8b59d6e7d
Thomas Dallamassl <Thomas.Dallamassl@A1.at>
2024-07-16 10:55:56 +0200

    splitted long code blocks into smaller methods

--- src/main/java/com/interns/simpleApp/controller/IndexController.java
+++ src/main/java/com/interns/simpleApp/controller/IndexController.java
@@ -29,66 +29,74 @@ public class IndexController {
 
     @RequestMapping("/registerInput")
     public String userRegistrationInput(@ModelAttribute User user, Model model) {
-        if(user.getFname().isEmpty() ||
-                user.getLname().isEmpty()  ||
-                user.getEmail().isEmpty()  ||
-                user.getPasswd().isEmpty()  ||
-                user.getPasswd2().isEmpty()  ||
-                user.getDob().isEmpty()
-        ) {
+        if (nonOptionalInputMissing(user)) {
             //NOI = Non-Optional Input
             model.addAttribute("NOI", "Please fill all non-optional boxes!");
             return "register";
-        } else if (!(user.getPasswd().equals(user.getPasswd2()))) {
+        }
+        if  (passwordsDontMatch(user)){
             //PDM = Passwords Don´t Match
             model.addAttribute("PDM", "Passwords don´t match!");
             return "register";
         }
-        for (User userEmail : users) {
-            if (userEmail.getEmail().equals(user.getEmail())) {
-                // EAU = Email Already Used
-                model.addAttribute("EAU", "An account with this email already exists!");
-                return "register";
-            }
+        if (emailAlreadyUsed(user)) {
+            // EAU = Email Already Used
+            model.addAttribute("EAU", "An account with this email already exists!");
+            return "register";
         }
-        if (users.size() +1 > maxUsers) {
+        if (userLimitReached()) {
             //ULR = User Limit Reached
             model.addAttribute("ULR", "User limit reached!");
             return "register";
-        } else {
-            if (user.getGender().equals("")) {
-                users.add(new User(
-                        user.getFname(),
-                        user.getLname(),
-                        user.getEmail(),
-                        user.getPasswd(),
-                        user.getPasswd2(),
-                        user.getDob(),
-                        "Not given"
-                ));
-            } else {
-                users.add(new User(
-                        user.getFname(),
-                        user.getLname(),
-                        user.getEmail(),
-                        user.getPasswd(),
-                        user.getPasswd2(),
-                        user.getDob(),
-                        user.getGender()
-                ));
-            }
         }
+
+        addUser(user);
         System.out.println(users.get(users.size() -1).toString());
         model.addAttribute("firstname", user.getFname());
         model.addAttribute("lastname", user.getLname());
         return "welcome";
     }
 
+    public boolean nonOptionalInputMissing(User user) {
+        return user.getFname().isEmpty() ||
+                user.getLname().isEmpty()  ||
+                user.getEmail().isEmpty()  ||
+                user.getPasswd().isEmpty()  ||
+                user.getPasswd2().isEmpty()  ||
+                user.getDob().isEmpty();
+    }
+
+    public boolean passwordsDontMatch(User user){
+        return (!(user.getPasswd().equals(user.getPasswd2())));
+    }
+
+    public boolean emailAlreadyUsed(User user){
+        for (User userEmail : users) {
+            return userEmail.getEmail().equals(user.getEmail());
+        }
+        return false;
+    }
+
+    public boolean userLimitReached(){
+        return users.size() +1 > maxUsers;
+    }
+
+    public boolean noGenderGiven(User user){
+        return user.getGender().isEmpty();
+    }
+
+    public void addUser(User user) {
+        if (noGenderGiven(user)) {
+            users.add(new User(user.getFname(), user.getLname(), user.getEmail(), user.getPasswd(), user.getPasswd2(), user.getDob(), "Not given"));
+        } else {
+            users.add(new User(user.getFname(), user.getLname(), user.getEmail(), user.getPasswd(), user.getPasswd2(), user.getDob(), user.getGender()));
+        }
+    }
 
     @RequestMapping("/loginInput")
     public String userLoginInput(@ModelAttribute LoginData login, Model model) {
         for (User userLogin : users) {
-            if (userLogin.getEmail().equals(login.getLogEmail()) && userLogin.getPasswd().equals(login.getLogPasswd())){
+            if (successfulLogin(login, userLogin)){
                 model.addAttribute("firstname", userLogin.getFname());
                 model.addAttribute("lastname", userLogin.getLname());
                 return "welcome";
@@ -97,6 +105,10 @@ public class IndexController {
         //NSA = No Such Account
         model.addAttribute("NSA", "No such account!");
         return "login";
+    }
+
+    public boolean successfulLogin(LoginData login, User userLogin){
+        return userLogin.getEmail().equals(login.getLogEmail()) && userLogin.getPasswd().equals(login.getLogPasswd());
     }
 
     @RequestMapping("/login")
