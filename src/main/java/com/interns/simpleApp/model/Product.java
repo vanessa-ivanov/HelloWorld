460f459986bbb5248f61558ce5522d729ab83975
vanessa-ivanov <vanessa.ivanov1@icloud.com>
2024-07-22 14:36:00 +0200

    ProductType enum created

--- src/main/java/com/interns/simpleApp/model/Product.java
+++ src/main/java/com/interns/simpleApp/model/Product.java
@@ -1,17 +1,50 @@
 package com.interns.simpleApp.model;
 
 public class Product {
-    String name;
-    String typ; // später enum erstellen
-    float price;
-    int amount;
+    private String name;
+    private ProductType typ; // später enum erstellen
+    private float price;
+    private int id;
 
-    public Product(String name, String typ, float price, int amount) {
+
+    private static int productId;
+
+    public Product(String name, ProductType typ, float price) {
         this.name = name;
         this.typ = typ;
         this.price = price;
-        this.amount = amount;
+        id = ++productId;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ProductType getTyp() {
+        return typ;
+    }
+
+    public void setTyp(ProductType typ) {
+        this.typ = typ;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
